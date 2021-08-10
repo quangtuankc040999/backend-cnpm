58f82eee800062b6e3d8d9b37ec65678c25084ab
DESKTOP-KMNS09Q\Admin <quangtuankc040999@gmail.com>
2021-08-11 00:23:11 +0700

    + active hotel

--- demo/src/main/java/com/example/demo/repository/HotelRepository.java
+++ demo/src/main/java/com/example/demo/repository/HotelRepository.java
@@ -2,9 +2,16 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface HotelRepository  extends JpaRepository<Hotel,Long > {
     Hotel findById (long id);
+
+    @Query(value = "SELECT * FROM hotel where is_active = 0;", nativeQuery = true)
+    public List<Hotel> getHotelIsActiveFalse();
+
 }
